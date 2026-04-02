298ff217ef0237bff1199267775a38896025ceec
Simon Lin <simonlin.rqmmw@slmail.me>
2026-04-02 23:25:27 +1100

    c

--- crates/polars-plan/src/plans/optimizer/ir_traversal/mod.rs
+++ crates/polars-plan/src/plans/optimizer/ir_traversal/mod.rs
@@ -1,4 +1,4 @@
 pub mod edge_provider;
 pub mod ir_graph;
 pub mod ir_node_key;
-pub mod tree_traversal;
+pub mod pullup_traversal;
---
+++ crates/polars-plan/src/plans/optimizer/ir_traversal/pullup_traversal.rs
@@ -0,0 +1,85 @@
+use polars_core::prelude::PlHashMap;
+use polars_error::PolarsResult;
+use polars_utils::arena::{Arena, Node};
+use polars_utils::{UnitVec, unitvec};
+
+use crate::plans::ir_traversal::edge_provider::IREdgeProvivder;
+use crate::plans::ir_traversal::ir_node_key::IRNodeKey;
+use crate::plans::{AExpr, IR};
+
+pub struct BasicEdgeProvider<'a, Edge> {
+    in_edges: &'a mut [Edge],
+    out_edges: &'a mut [Edge],
+}
+
+pub fn ir_pullup_traversal<Visitor, Edge: Default + Clone>(
+    roots: &[Node],
+    mut visitor_fn: Visitor,
+    ir_arena: &Arena<IR>,
+    expr_arena: &Arena<AExpr>,
+) -> PolarsResult<()>
+where
+    Visitor: FnMut(Node, &Arena<IR>, &Arena<AExpr>, &BasicEdgeProvider<Edge>) -> PolarsResult<()>,
+{
+    let mut visited_caches: PlHashMap<IRNodeKey, Edge> = PlHashMap::default();
+
+    for node in roots.iter() {
+        ir_pullup_traversal_rec(
+            *node,
+            &mut visitor_fn,
+            ir_arena,
+            expr_arena,
+            &mut visited_caches,
+            None,
+        );
+    }
+
+    Ok(())
+}
+
+pub fn ir_pullup_traversal_rec<Visitor, Edge: Default + Clone>(
+    node: Node,
+    visitor_fn: &mut Visitor,
+    ir_arena: &Arena<IR>,
+    expr_arena: &Arena<AExpr>,
+    visited_caches: &mut PlHashMap<IRNodeKey, Edge>,
+    out_edge: Option<&mut Edge>,
+) -> PolarsResult<()>
+where
+    Visitor: FnMut(Node, &Arena<IR>, &Arena<AExpr>, &BasicEdgeProvider<Edge>) -> PolarsResult<()>,
+{
+    let key = IRNodeKey::new(node, ir_arena);
+
+    if let Some(edge) = visited_caches.get(&key) {
+        *out_edge.unwrap() = edge.clone();
+        return Ok(());
+    }
+
+    let ir = ir_arena.get(node);
+    let mut in_edges: UnitVec<Edge> = ir.inputs().map(|_| Edge::default()).collect();
+
+    let edge_provider = BasicEdgeProvider {
+        in_edges: &mut in_edges,
+        out_edges: out_edge.map_or(&mut [], std::slice::from_mut),
+    };
+
+    for (node, edge) in ir.inputs().zip(edge_provider.in_edges.iter_mut()) {
+        ir_pullup_traversal_rec(
+            node,
+            visitor_fn,
+            ir_arena,
+            expr_arena,
+            visited_caches,
+            Some(edge),
+        )?;
+    }
+
+    visitor_fn(node, ir_arena, expr_arena, &edge_provider);
+
+    if matches!(ir_arena.get(node), IR::Cache { .. }) {
+        let existing = visited_caches.insert(key, todo!());
+        assert!(existing.is_none());
+    }
+
+    Ok(())
+}
